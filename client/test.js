7ef02766b796925c31aca139c6977773a49a22c1
Jeremy Castagno <JeremyBYU@gmail.com>
2015-12-14 22:03:36 -0600

    Remove unnecessary import

--- client/test.js
+++ client/test.js
@@ -1,7 +1,6 @@
 if (Meteor.isClient) {
   import moment from "moment"
-  import Formsy from 'formsy-react'
-  //  import {MmForm} from './components/mmform.jsx'
+
   let MmForm = require('./imports/mmform.jsx');
   //  console.log(MmForm);
 
